b615d8bb7ec4e7b601814aa47830e89e3e7e868d
Jonas Kruckenberg <jonas@kruckenbg.de>
2020-07-17 09:50:14 +0200

    test: move tests from mocha to jest

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,15 +1,13 @@
 import { expect } from 'chai'
 import sri from '../index'
-import 'mocha'
-import { OutputBundle } from 'rollup'
 
 describe('sri-plugin', () => {
-  it('is a function', () => {
-    expect(sri).to.be.a('function')
-  })
-  it('returns an object', () => {
-    expect(sri()).to.be.an('object')
-    expect(sri()).to.have.property('name').which.equals('rollup-plugin-sri')
-    expect(sri()).to.have.property('generateBundle').which.is.a('function')
-  })
+	it('is a function', () => {
+		expect(sri).to.be.a('function')
+	})
+	it('returns an object', () => {
+		expect(sri()).to.be.an('object')
+		expect(sri()).to.have.property('name').which.equals('rollup-plugin-sri')
+		expect(sri()).to.have.property('generateBundle').which.is.a('function')
+	})
 })
